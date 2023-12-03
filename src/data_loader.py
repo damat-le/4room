efeaa24c26be2c4aba0f50ef2518d7a2bbf0a9db
Leo D’Amato <leodamato@MacBook-Pro-di-Leo.local>
2023-12-03 23:48:06 +0100

    refactoring of data loading: in this new version, data can be loaded in parallel mode. Using 8 cores on my macbook, it take around 4 minutes to load the entire dataset.

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,86 +1,95 @@
 import os
-import multiprocessing as mp
 import pandas as pd
-import numpy as np
+import multiprocessing as mp
 
-class RatDataLoader:
-    def __init__(self, c, rat_name) -> None:
-        self.rat_name = rat_name
-        self.c = c
-        
-        self.rat_name = rat_name
-        bdata_dir = self.__get_ratdata_dir('b')
-        ndata_dir = self.__get_ratdata_dir('n')
-        self.bdata_dir = bdata_dir
-        self.ndata_dir = os.path.join(c["root_data_dir"], c["ndata_subdir"], rat_name)
 
-        sessions_toload = set(self.session_names).intersection(c["selected_sessions"])
-        
-    def __get_ratdata_dir(self, data_type):
-        if data_type=='n':
-            data_subdir = self.c["ndata_subdir"]
-        elif data_type=='b':
-            data_subdir = self.c["bdata_subdir"]
-        return os.path.join(
-            self.c["root_data_dir"], 
-            data_subdir, 
-            self.rat_name
-            )
+class DataLoader:
+
+    def __init__(self, c) -> None:
+        self.root_dir = c["root_data_dir"]
+        self.rat_names = c["rats_toload"]
+        self.session_names = c["sessions_toload"]
+        self.data_types = c["datatype_toload"]
+        self.n_jobs = c["n_jobs"]
+        self.targets = self.__get_targets()
+
+    def load(self):
+        self.data = self.__load_targets()
     
-    
-
-
-class BehaviourDataLoader:
-    """
-    For a given session, it loads the behaviour data.
-    """
-    def __init__(self, session_dir) -> None:
-        pass
-
-
-class BaseDataLoader:
-
-    
-
+    def __build_all_targets(self) -> dict:
+        targets = []
+        for path, dirs, files in os.walk(self.root_dir):
+            if files:
+                targets.append(path)
         
-        results = self.load_dirs_parallel(self.bdata_dir, n_jobs=c["n_jobs"])
+        df1 = pd.DataFrame(
+            targets, 
+            columns=['path']
+        )
+        tmp = df1.path.str.replace(self.root_dir+'/', '', regex=False).str.split('/').tolist()
+        for t in tmp:
+            if len(t) == 3:
+                t.append(None)
 
-        for session_name, session_dict in results:
-            self.__setattr__(
-                session_name, 
-                session_dict
-            )
+        df2 = pd.DataFrame(
+            tmp,
+            columns=['data_type', 'rat', 'session', 'cell']
+        )
+        #df2['cell'] = df2.cell.str.replace('.', '_', regex=False)
 
-    def load_dirs_parallel(self, in_dir, n_jobs=1) -> dict:
-        _,sub_dirs,_ = next(os.walk(in_dir))
-
-        # parallelize sub_dirs loading
-        sub_dirs = [os.path.join(in_dir, sub_dir) for sub_dir in sub_dirs if sub_dir.startswith("S")]
-
-        with mp.Pool(n_jobs) as pool:
-            results = pool.map(
+        df = pd.concat([df2, df1], axis=1)
+        df["hash"] = df.path.apply(hash)
+        
+        return df
+    
+    def __filter_targets(self, df) -> dict:
+        if self.rat_names is None:
+            self.rat_names = df.rat.unique()
+        if self.session_names is None:
+            self.session_names = df.session.unique()
+        if self.data_types is None:
+            self.data_types = df.data_type.unique()
+        return df[
+            df.rat.isin(self.rat_names) &
+            df.session.isin(self.session_names) &
+            df.data_type.isin(self.data_types)
+            ]
+    
+    def __get_targets(self) -> dict:
+        df = self.__build_all_targets()
+        df = self.__filter_targets(df)
+        return df
+    
+    def __load_targets(self) -> dict:
+        with mp.Pool(self.n_jobs) as pool:
+            data = pool.map(
                 self.load_files, 
-                sub_dirs
+                [(r.hash, r.path) for r in self.targets.itertuples()]
             )
-        
-        return results
+        return dict(data)
+    
+    @staticmethod
+    def load_files(in_tuple) -> tuple[int, dict]:
+        """
+        Load all files in a directory. 
 
-    def load_files(self, in_dir) -> dict:
+        The output is a container object with attributes corresponding to the filenames. The container has an ID attribute corresponding to the directory name.
+        """
+        _hash, in_dir = in_tuple
+
         out_dict = {}
         _,_,filenames = next(os.walk(in_dir))
         for filename in filenames:
             if filename.endswith(".csv"):
                 
                 try:
-                    attr_value = pd.read_csv(os.path.join(in_dir, filename), header=None).values
+                    attr_value = pd.read_csv(
+                        os.path.join(in_dir, filename), 
+                        header=None
+                    ).values
                 except:
                     print(f"attribute {filename} set to None")
-                    attr_value  = None   
+                    attr_value = None   
 
                 out_dict[filename[:-4]] = attr_value
-
-        #get name of parent folder
-        in_dir_name = os.path.basename(os.path.normpath(in_dir))
-                
-
-        return in_dir_name, out_dict
+        return _hash, out_dict
